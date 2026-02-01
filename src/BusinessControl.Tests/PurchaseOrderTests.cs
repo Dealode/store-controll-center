08022ee3f8f7cdbc8b7aaa128f7bb578d3ac1159
Vitalii Shmydko <karleonev@gmail.com>
2026-02-01 04:41:01 +0200

    add purchase order create test red

---
+++ src/BusinessControl.Tests/PurchaseOrderTests.cs
@@ -0,0 +1,25 @@
+namespace BusinessControl.Tests;
+
+[TestFixture]
+public class PurchaseOrderTests
+{
+    [Test]
+    public void Create_ShouldRaise_PurchaseOrderCreated_Event()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var supplierId = Guid.NewGuid();
+        
+        // Act
+        var order = PurchaseOrder.Create(id, supplierId);
+        
+        // Assert
+        order.Id.Should().Be(id);
+        order.SupplierId.Should().Be(supplierId);
+        order.Status.Should().Be(PurchaseOrderStatus.Draft);
+
+        var events = order.GetUncommittedEvents();
+        events.Should().ContainSingle()
+            .Which.Should().BeOfType<PurchaseOrderCreated>();
+    }
+}
