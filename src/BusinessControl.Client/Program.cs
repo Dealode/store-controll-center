939d18d59b724a2ba4839eaa9fadbb6a4b9e596e
Vitalii Shmydko <karleonev@gmail.com>
2026-02-23 02:21:02 +0200

    import MudBlazor

--- src/BusinessControl.Client/Program.cs
+++ src/BusinessControl.Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BusinessControl.Client;
+using BusinessControl.Client.Procurement;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new ProcurementApiClient(sp.GetRequiredService<HttpClient>()));
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
